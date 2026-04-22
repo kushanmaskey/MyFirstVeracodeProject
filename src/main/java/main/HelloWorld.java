08afe23e109fb8d35fa3c91ad319073054368f0a
Kushan Maskey <kushanmaskey@Kushans-MacBook-Pro.local>
2026-04-21 22:24:45 -0500

    Adding a test sayHello

--- src/main/java/main/HelloWorld.java
+++ src/main/java/main/HelloWorld.java
@@ -2,9 +2,17 @@ package main;
 
 public class HelloWorld {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hellow World 1");
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//		System.out.println("Hellow World 1");
+//	}
+	
+	HelloWorld() {
+		
+	}
+	
+	public void sayHello() {
+		System.out.println("Hello WOrld 1");
 	}
 
 }
